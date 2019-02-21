d95c5a384fd75db3bdfbaa436a4a076d0bf38889
John Vehikite <johncookv@gmail.com>
2019-02-21 01:06:56 -0800

    Fix footer padding and nav functionality

--- src/components/nav.js
+++ src/components/nav.js
@@ -14,7 +14,7 @@ export default (props) => (
         'overview',
         'get-involved',
         'plan',
-        'renderings',
+        // 'renderings',
         'news',
         'contact-us',
       ]}
@@ -23,34 +23,34 @@ export default (props) => (
     >
       {/* eslint-disable */}
       <li>
-        <Scroll type="id" element="overview" offset={-45}>
+        <Scroll type="id" element="overview" offset={0}>
           <a href="#">
             <a href="#">Overview</a>
           </a>
         </Scroll>
       </li>
       <li>
-        <Scroll type="id" element="get-involved" offset={-45}>
+        <Scroll type="id" element="get-involved" offset={0}>
           <a href="#">Get Involved</a>
         </Scroll>
       </li>
       <li>
-        <Scroll type="id" element="plan" offset={-45}>
+        <Scroll type="id" element="plan" offset={0}>
           <a href="#">Plan and Renderings</a>
         </Scroll>
       </li>
       {/* <li>
-        <Scroll type="id" element="renderings" offset={-45}>
+        <Scroll type="id" element="renderings" offset={0}>
           <a href="#">Renderings</a>
         </Scroll>
       </li> */}
       <li>
-        <Scroll type="id" element="news" offset={-45}>
+        <Scroll type="id" element="news" offset={0}>
           <a href="#">News</a>
         </Scroll>
       </li>
       <li>
-        <Scroll type="id" element="contact-us" offset={-45}>
+        <Scroll type="id" element="contact-us" offset={0}>
           <a href="#">Contact Us</a>
         </Scroll>
       </li>
