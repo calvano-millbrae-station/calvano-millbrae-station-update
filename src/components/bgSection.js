ef0563d91b2108de43a4fe8d9330d5c95e0bcc8e
John Vehikite <johncookv@gmail.com>
2019-02-24 21:21:33 -0800

    Fix small issues

--- src/components/bgSection.js
+++ src/components/bgSection.js
@@ -1,8 +1,5 @@
 import React from 'react'
 
 export default (props) => (
-  <div className={`bg-section bg-image${props.bgImageNum}`}>
-    <div className="bg-mask">
-    </div>
-  </div>
+  <div className={`bg-section bg-image${props.bgImageNum}`} />
 )
--- src/components/hero.js
+++ src/components/hero.js
@@ -18,12 +18,6 @@ export default (props) => (
     render={data => {
       const imageData = data.hero.childImageSharp.fluid
       return (
-        // <div className={`hero ${props.isHome ? "" : "not-home-hero"}`}>
-        //   <div className="content">
-        //     <img src={require("../assets/images/millbrae-logo.png")} alt="" />
-        //     <span>{props.isAdmin ? "Admin Page" : "Millbrae Station"}</span>
-        //   </div>
-        // </div>
         <BackgroundImage
           Tag="section"
           className={`hero ${props.isHome ? "" : "not-home-hero"}`}
