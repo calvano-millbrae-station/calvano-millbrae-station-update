ac1e73045dcf469b1c99339b9002fe75e6b2e389
John Vehikite <johncookv@gmail.com>
2019-03-12 22:49:30 -0700

    Implement contentful

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase'
 const config = {
-  apiKey: "process.env.FIREBASE_API_KEY",
+  apiKey:process.env.FIREBASE_API_KEY,
   authDomain: "calvano-millbrae-station.firebaseapp.com",
   databaseURL: "https://calvano-millbrae-station.firebaseio.com",
   projectId: "calvano-millbrae-station",
   storageBucket: "calvano-millbrae-station.appspot.com",
-  messagingSenderId: "process.env.FIREBASE_MESSAGING_SENDER_ID"
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 export default firebase;
